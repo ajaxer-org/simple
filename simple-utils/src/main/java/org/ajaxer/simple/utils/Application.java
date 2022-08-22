1a56bc1319b69c359fe09a645429bb2905fa36c8
Shakir Ansari <shakir.ansari@live.com>
2022-08-22 21:23:39 +0530

    Update Application.java

--- simple-utils/src/main/java/org/ajaxer/simple/utils/Application.java
+++ simple-utils/src/main/java/org/ajaxer/simple/utils/Application.java
@@ -5,11 +5,14 @@ import lombok.extern.log4j.Log4j2;
 /**
  * @author Shakir
  * @version 2022-08-22
- * @since 0.0.1
+ * @since v0.0.1
  */
 @Log4j2
 public class Application
 {
+	/**
+	 * @since v0.0.1
+	 */
 	public static void main(String[] args)
 	{
 		log.info("Hello World");
