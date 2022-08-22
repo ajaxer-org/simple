aeac65ed5ff76b7bc46fd49f559495d57856e4e9
Shakir Ansari <shakir.ansari@live.com>
2022-08-22 21:12:52 +0530

    added module simple-utils

---
+++ simple-utils/src/main/java/org/ajaxer/simple/utils/SimpleUtils.java
@@ -0,0 +1,17 @@
+package org.ajaxer.simple.utils;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author Shakir
+ * @version 2022-08-22
+ * @since 0.0.1
+ */
+@Log4j2
+public class SimpleUtils
+{
+	public static void main(String[] args)
+	{
+		log.info("Hello World");
+	}
+}
