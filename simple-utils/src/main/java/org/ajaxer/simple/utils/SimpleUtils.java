f6b6d1f3227f46b9d9fbbbf620311a59ddca239b
Shakir Ansari <shakir.ansari@live.com>
2022-08-23 22:36:09 +0530

    Create SimpleUtils.java

---
+++ simple-utils/src/main/java/org/ajaxer/simple/utils/SimpleUtils.java
@@ -0,0 +1,53 @@
+package org.ajaxer.simple.utils;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author Shakir
+ * @version 2022-08-23
+ * @since v0.0.1
+ */
+@Log4j2
+public class SimpleUtils
+{
+	/**
+	 * @since v0.0.1
+	 */
+	public static void close(AutoCloseable autoCloseable)
+	{
+		try
+		{
+			autoCloseable.close();
+			log.debug("autoCloseable.close() executed successfully: {}", autoCloseable.toString());
+		} catch (Exception exception)
+		{
+			log.warn("autoCloseable.close() executed unsuccessfully: {}", autoCloseable.toString());
+		}
+	}
+
+	/**
+	 * @since v0.0.1
+	 */
+	public static void sleep(long mili)
+	{
+		if (mili <= 0)
+		{
+			return;
+		}
+
+		try
+		{
+			Thread.sleep(mili);
+		} catch (InterruptedException ignored)
+		{
+		}
+	}
+
+	/**
+	 * @since v0.0.1
+	 */
+	public static void sleep()
+	{
+		sleep(2000);
+	}
+}
