9db762288d55f3ae0df37709759b558d596e7d1d
Shakir Ansari <shakir.ansari@live.com>
2022-08-27 18:59:30 +0530

    Create ContentType.java

---
+++ simple-utils/src/main/java/org/ajaxer/simple/utils/constants/ContentType.java
@@ -0,0 +1,49 @@
+package org.ajaxer.simple.utils.constants;
+
+/**
+ * @author Shakir
+ * @version 2022-08-27
+ * @since v0.0.1
+ */
+@SuppressWarnings("unused")
+public class ContentType
+{
+	/*
+	 * Standard character encoding
+	 */
+	public static final String ISO_8859_1 = "ISO-8859-1";
+	public static final String UTF_8 = "UTF-8";
+
+	/*
+	 * Standard content type
+	 */
+	public static final String APPLICATION_ATOM_XML = "application/atom+xml";
+	public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+	public static final String APPLICATION_JSON = "application/json";
+	public static final String APPLICATION_JAVASCRIPT = "application/javascript";
+	public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+	public static final String APPLICATION_PDF = "application/pdf";
+	public static final String APPLICATION_SVG_XML = "application/svg+xml";
+	public static final String APPLICATION_XHTML_XML = "application/xhtml+xml";
+	public static final String APPLICATION_XML = "application/xml";
+	public static final String APPLICATION_X_GZIP = "application/x-gzip";
+
+	public static final String AUDIO_MP3 = "audio/mpeg";
+
+	public static final String IMAGE_GIF = "image/gif";
+	public static final String IMAGE_JPG = "image/jpg";
+	public static final String IMAGE_JPEG = "image/jpeg";
+	public static final String IMAGE_PNG = "image/png";
+
+	public static final String SPREADSHEET_XLS = "application/vnd.ms-excel";
+	public static final String SPREADSHEET_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+	public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+
+	public static final String TEXT_CSS = "text/css";
+	public static final String TEXT_HTML = "text/html";
+	public static final String TEXT_PLAIN = "text/plain";
+	public static final String TEXT_XML = "text/xml";
+
+	public static final String WILDCARD = "*/*";
+}
