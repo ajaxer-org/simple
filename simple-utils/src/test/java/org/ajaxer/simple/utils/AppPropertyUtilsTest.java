63c543f629cd3811e9b3ef1c7c702e9c2e1a6824
Shakir Ansari <shakir.ansari@live.com>
2022-08-27 18:59:39 +0530

    Create AppPropertyUtilsTest.java

---
+++ simple-utils/src/test/java/org/ajaxer/simple/utils/AppPropertyUtilsTest.java
@@ -0,0 +1,95 @@
+package org.ajaxer.simple.utils;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@Log4j2
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AppPropertyUtilsTest
+{
+	private AppPropertyUtils propertyUtils;
+
+	@BeforeAll
+	void beforeAll()
+	{
+		try
+		{
+			propertyUtils = new AppPropertyUtils("app.properties");
+		} catch (Exception exception)
+		{
+			ExceptionUtils.rethrow(exception);
+		}
+	}
+
+	@AfterAll
+	void afterAll()
+	{
+		SimpleUtils.close(propertyUtils);
+	}
+
+	@Test
+	public void getKeyWhenKeyIsBlank()
+	{
+		Assertions.assertThrows(NullPointerException.class, () -> propertyUtils.getKey(null));
+		Assertions.assertThrows(NullPointerException.class, () -> propertyUtils.getKey(""));
+	}
+
+	@Test
+	public void getKey()
+	{
+		String value = propertyUtils.getKey("foo");
+		String expected = "bar";
+
+		Assertions.assertEquals(expected, value);
+	}
+
+	@Test
+	public void getInt()
+	{
+		int value = propertyUtils.getInt("number");
+		log.info("value: {}", value);
+		int expected = 123;
+
+		Assertions.assertEquals(expected, value);
+	}
+
+	@Test
+	public void getIntWithDefaultValues()
+	{
+		int value = propertyUtils.getInt("KEY_DOES_NOT_EXISTS", 9);
+		log.info("value: {}", value);
+		int expected = 9;
+
+		Assertions.assertEquals(expected, value);
+	}
+
+	@Test
+	public void setKey()
+	{
+		propertyUtils.setKey("foo1", "bar1");
+		String value = propertyUtils.getKey("foo1");
+		String expected = "bar1";
+
+		Assertions.assertEquals(expected, value);
+
+	}
+
+	@Test
+	public void containKey()
+	{
+		Assertions.assertEquals(true, propertyUtils.containKey("number"));
+		Assertions.assertEquals(false, propertyUtils.containKey("KEY_DOES_NOT_EXISTS"));
+	}
+
+	@Test
+	public void containsValue()
+	{
+		Assertions.assertEquals(true, propertyUtils.containsValue("123"));
+		Assertions.assertEquals(false, propertyUtils.containsValue("VALUE_DOES_NOT_EXISTS"));
+	}
+
+}
