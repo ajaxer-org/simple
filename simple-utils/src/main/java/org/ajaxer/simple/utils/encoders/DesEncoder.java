619bab11bfec5b2d5f01275104f612c4e8817f6a
Shakir Ansari <shakir.ansari@live.com>
2022-09-06 12:47:27 +0530

    DesEncoder and DesEncoderTest

--- simple-utils/src/main/java/org/ajaxer/simple/utils/encoders/DesEncoder.java
+++ simple-utils/src/main/java/org/ajaxer/simple/utils/encoders/DesEncoder.java
@@ -4,8 +4,10 @@ import org.ajaxer.simple.utils.ExceptionUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 /**
@@ -20,7 +22,7 @@ public class DesEncoder implements Encoder
 	private static SecretKey secretKey;
 	private static Cipher cipher;
 
-	private static void init() throws Exception
+	private static void init() throws NoSuchAlgorithmException, NoSuchPaddingException
 	{
 		if (keygenerator == null)
 		{
@@ -41,7 +43,8 @@ public class DesEncoder implements Encoder
 	@Override
 	public String encode(final String message)
 	{
-		ExceptionUtils.throwWhenBlank(message);
+		if (message == null || message.isEmpty()) return null;
+
 		try
 		{
 			init();
@@ -54,29 +57,28 @@ public class DesEncoder implements Encoder
 
 		} catch (Exception exception)
 		{
-			ExceptionUtils.rethrow(exception);
-			return null;
+			return ExceptionUtils.rethrow(exception, String.class);
 		}
 	}
 
 	@Override
 	public String decode(final String message)
 	{
-		ExceptionUtils.throwWhenBlank(message);
+		if (message == null || message.isEmpty()) return null;
+
 		try
 		{
 			init();
 
 			cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-			byte[] cipherBytes = cipher.doFinal(Base64.getDecoder().decode(message));
+			byte[] cipherBytes = cipher.doFinal(Base64.getDecoder().decode(message.getBytes()));
 
 			return new String(cipherBytes);
 
 		} catch (Exception exception)
 		{
-			ExceptionUtils.rethrow(exception);
-			return null;
+			return ExceptionUtils.rethrow(exception, String.class);
 		}
 	}
 }
