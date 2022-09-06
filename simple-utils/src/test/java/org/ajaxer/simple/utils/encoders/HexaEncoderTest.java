6e35eaea3d868df2b94f35f130724ac87f02b348
Shakir Ansari <shakir.ansari@live.com>
2022-09-06 12:08:35 +0530

    Create HexaEncoderTest.java

---
+++ simple-utils/src/test/java/org/ajaxer/simple/utils/encoders/HexaEncoderTest.java
@@ -0,0 +1,14 @@
+package org.ajaxer.simple.utils.encoders;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author Shakir
+ * @version 2022-09-05
+ * @since v0.0.1
+ */
+@Log4j2
+public class HexaEncoderTest
+{
+
+}
