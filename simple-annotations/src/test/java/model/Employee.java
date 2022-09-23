129a35b66681c61604eafdcef1642aad94760528
Shakir Ansari <shakir.ansari@live.com>
2022-09-23 18:42:36 +0530

    test models

---
+++ simple-annotations/src/test/java/model/Employee.java
@@ -0,0 +1,21 @@
+package model;
+
+import org.ajaxer.simple.annotations.Builder;
+
+/**
+ * @author Shakir
+ * @version 2022-09-18
+ */
+@Builder
+public class Employee
+{
+	private String name;
+	private int age;
+
+	public Employee(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+}
+
---
+++ simple-annotations/src/test/java/model/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Shakir
+ * @version 2022-09-18
+ */
+public class HelloWorldTest
+{
+	@Test
+	void name()
+	{
+		Assertions.assertEquals(1, 1);
+	}
+}
