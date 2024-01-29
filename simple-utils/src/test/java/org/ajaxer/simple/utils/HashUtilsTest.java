370f9970af58cd7790777899a74a1394310bb43d
Shakir Ansari <shakir.ansari@live.com>
2024-01-29 19:26:58 +0530

    added copyrights

--- simple-utils/src/test/java/org/ajaxer/simple/utils/HashUtilsTest.java
+++ simple-utils/src/test/java/org/ajaxer/simple/utils/HashUtilsTest.java
@@ -1,5 +1,21 @@
 package org.ajaxer.simple.utils;
 
+/*
+ * Copyright (c) 2024 ajaxer.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
