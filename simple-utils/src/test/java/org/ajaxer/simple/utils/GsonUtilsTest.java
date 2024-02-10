2f5a56be00b0b55b287d665774c04bbd7b821b82
Shakir Ansari <shakir.ansari@live.com>
2024-02-10 21:17:29 +0530

    added GsonUtils & deprecated JsonUtils

---
+++ simple-utils/src/test/java/org/ajaxer/simple/utils/GsonUtilsTest.java
@@ -0,0 +1,228 @@
+package org.ajaxer.simple.utils;
+
+/*
+ * Copyright (c) 2024 ajaxer.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import lombok.extern.log4j.Log4j2;
+import org.ajaxer.simple.utils.dtos.UserDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Shakir
+ * @version 2024-02-10
+ * @since v0.3.0
+ */
+@Log4j2
+public class GsonUtilsTest
+{
+	private UserDto mUserDto = new UserDto("ajaxer", "email@ajaxer.org", "uuid-123-4567-890");
+	private String mUserDtoJson = "{\"name\":\"ajaxer\",\"email\":\"email@ajaxer.org\",\"uuid\":\"uuid-123-4567-890\"}";
+
+	@Nested
+	class GetPrettyJson
+	{
+		@Test
+		void getPrettyJson_with_blank_string()
+		{
+			Assertions.assertNull(GsonUtils.getPrettyJson(null));
+			Assertions.assertNull(GsonUtils.getPrettyJson(""));
+		}
+
+		@Test
+		void getPrettyJson_with_non_json_string()
+		{
+			String nonJson = StringUtils.getUUID();
+			log.debug("nonJson: {}", nonJson);
+
+			String response = GsonUtils.getPrettyJson(nonJson);
+			log.debug("response: {}", response);
+
+			String expected = "\"" + nonJson + "\"";
+			log.debug("expected: {}", expected);
+
+			Assertions.assertEquals(expected, response);
+		}
+
+		@Test
+		void getPrettyJson_with_correct_json_string()
+		{
+			String response = GsonUtils.getPrettyJson(mUserDtoJson);
+			log.info("response: {}", response);
+
+			UserDto responseDto = GsonUtils.toObject(response, UserDto.class);
+			log.info("responseDto: {}", responseDto);
+
+			Assertions.assertEquals(mUserDto, responseDto);
+		}
+	}
+
+	@Nested
+	class ToJsonStringToObject
+	{
+		@Test
+		void toJsonString_with_null_object()
+		{
+			Assertions.assertNull(GsonUtils.toJsonString(null));
+		}
+
+		@Test
+		void toObject_with_blank_json_and_null_type()
+		{
+			Assertions.assertNull(GsonUtils.toObject(null, null));
+			Assertions.assertNull(GsonUtils.toObject("", null));
+		}
+
+		@Test
+		void toJsonString_toObject()
+		{
+			String userDtoString = GsonUtils.toJsonString(mUserDto);
+			log.info("userDtoString: {}", userDtoString);
+
+			UserDto userDtoResult = GsonUtils.toObject(userDtoString, UserDto.class);
+			log.info("userDtoResult: {}", userDtoResult);
+
+			Assertions.assertEquals(mUserDto, userDtoResult);
+		}
+	}
+
+	@Nested
+	class ToObjectList
+	{
+		@Test
+		void toObjectList_with_blank_json_and_null_type()
+		{
+			Assertions.assertNull(GsonUtils.toObjectList(null, null));
+			Assertions.assertNull(GsonUtils.toObjectList("", null));
+		}
+
+		@Test
+		void toObjectList_with_correct_data()
+		{
+			List<UserDto> userDtos = Arrays.asList(
+					new UserDto("name0", "email0", "uuid0"),
+					new UserDto("name1", "email1", "uuid1"),
+					new UserDto("name2", "email2", "uuid2")
+			);
+			String json = GsonUtils.toJsonString(userDtos);
+			log.info("json: {}", json);
+
+			List<UserDto> userDtosResult = GsonUtils.toObjectList(json, UserDto.class);
+			log.info("userDtosResult: {}", userDtosResult);
+
+			for (int i = 0; i < 3; i++)
+			{
+				Assertions.assertEquals(userDtos.get(i).getName(), userDtosResult.get(i).getName());
+				Assertions.assertEquals(userDtos.get(i).getEmail(), userDtosResult.get(i).getEmail());
+				Assertions.assertEquals(userDtos.get(i).getUuid(), userDtosResult.get(i).getUuid());
+				Assertions.assertEquals(userDtos.get(i), userDtosResult.get(i));
+			}
+		}
+	}
+
+	@Nested
+	class Parse
+	{
+		@Test
+		void parse_when_jsonString_blank()
+		{
+			Assertions.assertNull(GsonUtils.parse(null));
+			Assertions.assertNull(GsonUtils.parse(""));
+		}
+
+		@Test
+		void parse_when_correct_args_t1()
+		{
+			JsonObject jsonObject = GsonUtils.parse(mUserDtoJson);
+			log.info("jsonObject: {}", jsonObject);
+
+			Assertions.assertEquals(jsonObject.getAsJsonPrimitive("name").getAsString(), "ajaxer");
+		}
+
+		@Test
+		void parse_when_correct_args_t2()
+		{
+			JsonObject jsonObject = GsonUtils.parse(mUserDtoJson);
+			log.info("jsonObject: {}", jsonObject);
+			String actual = "email@ajaxer.org";
+
+			Assertions.assertEquals(jsonObject.getAsJsonPrimitive("email").getAsString(), actual);
+		}
+	}
+
+	@Nested
+	class GetJsonArray
+	{
+		@Test
+		void getJsonArray_when_args_null_blank()
+		{
+			Assertions.assertNull(GsonUtils.getJsonArray(null, null));
+			Assertions.assertNull(GsonUtils.getJsonArray("", ""));
+			Assertions.assertNull(GsonUtils.getJsonArray("", null));
+			Assertions.assertNull(GsonUtils.getJsonArray(null, ""));
+		}
+
+		@Test
+		void getJsonArray_with_correct_data()
+		{
+			HashMap<String, Object> hashMap = new HashMap<>();
+
+			List<UserDto> userDtos = Arrays.asList(
+					new UserDto("name0", "email0", "uuid0"),
+					new UserDto("name1", "email1", "uuid1"),
+					new UserDto("name2", "email2", "uuid2")
+			);
+
+			hashMap.put("userDtos", userDtos);
+			hashMap.put("foo", "bar");
+
+			String json = GsonUtils.toJsonString(hashMap);
+			log.info("json: {}", json);
+
+			JsonArray array = GsonUtils.getJsonArray(json, "userDtos");
+			for (int i = 0; i < array.size(); i++)
+			{
+				log.info("jsonElement: {}", array.get(i));
+
+				String result = array.get(i).getAsJsonObject().getAsJsonPrimitive("email").getAsString();
+				String expected = userDtos.get(i).getEmail();
+
+				Assertions.assertEquals(result, expected);
+			}
+		}
+	}
+
+	@Nested
+	class GetElementAs
+	{
+		@Test
+		void getElementAsType_when_args_null_blank()
+		{
+			Assertions.assertNull(GsonUtils.getElementAsType(null, null, null));
+			Assertions.assertNull(GsonUtils.getElementAsType("", null, ""));
+			Assertions.assertNull(GsonUtils.getElementAsType("", null, null));
+			Assertions.assertNull(GsonUtils.getElementAsType(null, null, ""));
+		}
+	}
+
+}
